db2c392b3867acfe5eb9b271643d5b6b783be254
Ludowik <ludovic.milhau@gmail.com>
2026-06-19 16:54:08 +0200

    fix: retire build-wasm/ du dépôt (artefacts de build)

--- build-wasm/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lca/Dev/ollin/build-wasm/_deps/raylib-src/src
-# Build directory: /Users/lca/Dev/ollin/build-wasm/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
